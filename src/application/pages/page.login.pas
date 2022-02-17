be4aac45b2d7a8ad95e215744054a5e4f44513f7
alemedeiros@bsd.com.br <alepmedeiros>
2022-02-17 12:50:59 -0300

    melhorias no layout

--- src/application/pages/page.login.pas
+++ src/application/pages/page.login.pas
@@ -4,10 +4,21 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.WinXPanels,
+  Vcl.Imaging.jpeg, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+    Panel2: TPanel;
+    StackPanel1: TStackPanel;
+    Label1: TLabel;
+    Edit1: TEdit;
+    Label2: TLabel;
+    Edit2: TEdit;
+    Button1: TButton;
+    Label3: TLabel;
   private
     { Private declarations }
   public
