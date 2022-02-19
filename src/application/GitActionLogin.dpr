279f60f1d86c7d12b4218092aaea13abbf2341a0
alemedeiros@bsd.com.br <alepmedeiros>
2022-02-18 22:51:34 -0300

    Melhorias no layout

--- src/application/GitActionLogin.dpr
+++ src/application/GitActionLogin.dpr
@@ -2,7 +2,7 @@ program GitActionLogin;
 
 uses
   Vcl.Forms,
-  page.login in 'pages\page.login.pas' {Form1},
+  page.login in 'pages\page.login.pas' {PageLogin},
   page.cadastro in 'pages\page.cadastro.pas' {pagecadastro};
 
 {$R *.res}
@@ -10,7 +10,7 @@ uses
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TPageLogin, PageLogin);
   Application.CreateForm(Tpagecadastro, pagecadastro);
   Application.Run;
 end.
--- src/application/pages/page.login.pas
+++ src/application/pages/page.login.pas
@@ -15,20 +15,57 @@ uses
   Vcl.StdCtrls,
   Vcl.WinXPanels,
   Vcl.Imaging.jpeg,
-  Vcl.ExtCtrls;
+  Vcl.ExtCtrls,
+  LocalCache4D, PanelTransparent, Vcl.Imaging.pngimage;
 
 type
-  TForm1 = class(TForm)
+  TPageLogin = class(TForm)
     Panel1: TPanel;
     Image1: TImage;
-    pnlContainer: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel6: TPanel;
+    StackPanel2: TStackPanel;
     StackPanel1: TStackPanel;
-    Label1: TLabel;
-    edtNome: TEdit;
-    Label2: TLabel;
-    edtSenha: TEdit;
+    StackPanel3: TStackPanel;
+    Panel9: TPanel;
+    Image2: TImage;
+    Edit1: TEdit;
+    Panel8: TPanel;
+    Image3: TImage;
+    Edit2: TEdit;
     Button1: TButton;
+    Label1: TLabel;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    StackPanel4: TStackPanel;
+    Button2: TButton;
+    Panel7: TPanel;
+    Panel10: TPanel;
+    Image4: TImage;
+    Label2: TLabel;
+    Panel11: TPanel;
     Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Panel12: TPanel;
+    Label8: TLabel;
+    StackPanel5: TStackPanel;
+    Panel13: TPanel;
+    Image8: TImage;
+    Panel14: TPanel;
+    Image7: TImage;
+    Panel15: TPanel;
+    Image6: TImage;
+    Panel16: TPanel;
+    Image5: TImage;
+    procedure Edit2Click(Sender: TObject);
+    procedure Edit1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -36,10 +73,26 @@ type
   end;
 
 var
-  Form1: TForm1;
+  PageLogin: TPageLogin;
 
 implementation
 
 {$R *.dfm}
+
+
+procedure TPageLogin.Edit1Click(Sender: TObject);
+begin
+  Edit1.Clear;
+  Edit1.Font.Color := clBlack;
+  Edit1.PasswordChar := '*';
+  Image2.Visible := false;
+end;
+
+procedure TPageLogin.Edit2Click(Sender: TObject);
+begin
+  Edit2.Clear;
+  Edit2.Font.Color := clBlack;
+  Image3.Visible := false;
+end;
 
 end.
